b624a6cd4dab4418d2d875419aa47ea88ef82c11
Thacher Shields <thacher@mac.com>
2017-10-17 10:57:37 -0400

    Added color palette

--- app/src/main/java/com/thebeerdudes/thacher/roughdraught/MainActivity.java
+++ app/src/main/java/com/thebeerdudes/thacher/roughdraught/MainActivity.java
@@ -1,7 +1,12 @@
 package com.thebeerdudes.thacher.roughdraught;
 
+import android.app.Activity;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
     }
 }
